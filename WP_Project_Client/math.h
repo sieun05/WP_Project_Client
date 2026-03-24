6af6b8c896380363df92f512a36850755970517c
OhSieun <sieun125@tukorea.ac.kr>
2026-03-24 20:19:49 +0900

    Cell, Vec2, Direction 구조체변수 선언한 헤더파일

---
+++ WP_Project_Client/math.h
@@ -0,0 +1,27 @@
+#pragma once
+
+struct Cell {
+	Cell() = default;
+	Cell(int type) : type(type), block_num(0) {}
+	Cell(int type, int block_num) : type(type), block_num(block_num) {}
+	Cell(const Cell& other) = default;
+	Cell& operator=(const Cell& other) = default;
+
+	int type;
+	int block_num;
+};
+
+struct Vec2 {
+	int x, y;
+}; 
+
+enum Direction {
+	DIR_DOWN = 0,       // 아래
+	DIR_DOWN_RIGHT,     // 아래-오른쪽 대각선
+	DIR_RIGHT,          // 오른쪽
+	DIR_UP_RIGHT,       // 위-오른쪽 대각선
+	DIR_UP,             // 위
+	DIR_UP_LEFT,        // 위-왼쪽 대각선
+	DIR_LEFT,           // 왼쪽
+	DIR_DOWN_LEFT       // 아래-왼쪽 대각선
+};
