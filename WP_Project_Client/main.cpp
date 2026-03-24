af7cea75917b628e72f616fba0cfa89e926a212a
OhSieun <sieun125@tukorea.ac.kr>
2026-03-24 20:20:49 +0900

    main.cpp 하얀 창만 켜짐

---
+++ WP_Project_Client/main.cpp
@@ -0,0 +1,83 @@
+#include <windows.h>
+#include <tchar.h>
+#include <stdio.h>
+#include <map>
+#include <string>
+#include <stdlib.h>
+#include <random>
+#include <wingdi.h>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <array>
+
+#include "map.h"
+#include "Player.h"
+
+#pragma comment(lib, "Msimg32.lib")
+
+using namespace std;
+
+#define marginX 16
+#define marginY 39
+
+HINSTANCE g_hInst;
+LPCTSTR lpszClass = _T("MyWindowClass");
+LPCTSTR lpszWindowName = _T("Client");
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam);
+
+int  WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_  LPSTR lpszCmdParam, _In_  int nCmdShow)
+{
+	HWND hWnd;
+	MSG Message;
+	WNDCLASSEX WndClass;
+	g_hInst = hInstance;
+
+	WndClass.cbSize = sizeof(WndClass);
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	WndClass.lpfnWndProc = (WNDPROC)WndProc;
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hInstance = hInstance;
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hCursor = LoadCursor(NULL, IDI_APPLICATION);
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.lpszMenuName = NULL;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	RegisterClassEx(&WndClass);
+
+	//--- 크기 변경 가능 (기존 (1024, 768))
+	hWnd = CreateWindow(
+		lpszClass,
+		lpszWindowName,
+		WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
+		0, 0,
+		1280,
+		720,
+		NULL,
+		(HMENU)NULL,
+		hInstance,
+		NULL);
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	while (GetMessage(&Message, 0, 0, 0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return Message.wParam;
+}
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+{
+
+	switch (iMessage) {
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hWnd, iMessage, wParam, lParam);
+}
