c7a4a8913725aca97602d27e69fba4450b1b6355
OhSieun <sieun125@tukorea.ac.kr>
2026-03-24 21:01:18 +0900

    맵, 플레이어매니저 클래스로 작성

--- WP_Project_Client/PlayerManager.h
+++ WP_Project_Client/PlayerManager.h
@@ -1,11 +1,43 @@
 #pragma once
-#include <vector>
-#include <WinSock2.h>
-#include <deque>
+#include <unordered_map>
 #include "Player.h"
 
 class PlayerManager 
 {
 public:
-	Player* AddPlayer()
+	void AddofUpdatePlayer(uint32_t id, int x, int y) {
+		auto it = players.find(id);
+		if (it != players.end()) {
+			it->second.SetPos({ x, y });
+		}
+		else {
+			players.emplace(id, Player(id, { x, y }, 0));
+		}
+	};
+	void RemovePlayer(uint32_t id) {
+		players.erase(id);
+	};
+
+	Player* GetPlayer(uint32_t id) {
+		auto it = players.find(id);
+		if (it != players.end()) {
+			return &it->second;
+		}
+		return nullptr;
+	}
+	void Update(float deltaTime) {
+		for (auto& pair : players) {
+			pair.second.Update(deltaTime);
+		}
+	}
+	void Render() {
+		for (auto& pair : players) {
+			pair.second.Render();
+		}
+	}
+
+private:
+	std::unordered_map<uint32_t, Player> players;
 };
+
+PlayerManager playerManager;
--- WP_Project_Client/map.h
+++ WP_Project_Client/map.h
@@ -4,4 +4,24 @@
 #define MAP_WIDTH 100
 #define MAP_HEIGHT 100
 
-Cell game_map[MAP_HEIGHT][MAP_WIDTH]{};
+class TileMap 
+{
+public:
+	void Render() {
+		// 타일맵 렌더링 로직 (예: Direct2D를 사용하여 화면에 그리기)
+	}
+	Cell& GetCell(int x, int y) { return game_map[y][x]; }
+	void SetCell(int x, int y, const Cell& cell) { game_map[y][x] = cell; }
+	void LoadMap(const Cell new_map[MAP_HEIGHT][MAP_WIDTH]) {
+		for (int y = 0; y < MAP_HEIGHT; ++y) {
+			for (int x = 0; x < MAP_WIDTH; ++x) {
+				game_map[y][x] = new_map[y][x];
+			}
+		}
+	}
+
+private:
+	Cell game_map[MAP_HEIGHT][MAP_WIDTH]{};
+};
+
+TileMap map;
